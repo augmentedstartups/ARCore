6eeea110dace434bbe6e2fb388ac5a5aa9747100
Augmented Startups <reigngt09@gmail.com>
2018-05-14 17:29:59 +0200

    Updated for ARCore 1.2
    #Assets for HitTesting tutorial using ARCore 1.2.
    
    ChangeLog - TrackedPlane Prefab has now been renamed to DetectedPlaneVisualizer
    and can be found at:
    GoogleARCore>>Examples>>Common>>Prefabs>>DetectedPlaneVisualizer
    Use the PlaneVisualizationManager.cs from the ARCore1.2 Folder
    InstantiatObjectOnTouch.cs Code Changed => GoogleARCore.HelloAR; to GoogleARCore.Examples.HelloAR; 
    
    If you are using ARCore 1.1 or 1.0, You will find this at:
    GoogleARCore>>Examples>>HelloAR>>Prefabs>>TrackedPlaneVisualizer
    Use the PlaneVisualizationManager.cs from the ARCore1.0_1.1 Folder

---
+++ B_HitTestingARCore/ARCore1.2/InstantiateObjectOnTouch.cs
@@ -0,0 +1,44 @@
+﻿/*================================================
+==Adapted by  : Ritesh Kanjee(Arduino Startups)	==
+==Date        : 12 March 2018      				==
+==Revision    : 1.2 				 		  	==	
+==Description : The gameobject to place when	==
+==			    tapping the screen.				==
+==			   							  		==	
+==================================================*/
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.HelloAR;
+
+public class InstantiateObjectOnTouch : MonoBehaviour { 
+	public Camera FirstPersonCamera; // The first-person camera being used to render the passthrough camera
+    public GameObject PlaceGameObject;
+	// Update is called once per frame
+	void Update ()
+	{
+		// Get the touch position from the screen to see if we have at least one touch event currently active
+		Touch touch;
+		if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+		{
+			return;
+		}
+		// Now that we know that we have an active touch point, do a raycast to see if it hits
+		// a plane where we can instantiate the object on.
+		TrackableHit hit;
+		var raycastFilter = TrackableHitFlags.PlaneWithinBounds | TrackableHitFlags.PlaneWithinPolygon;
+
+		if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit) && PlaceGameObject != null)
+		{
+			// Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+			// world evolves.
+			var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            // Instantiate a game object as a child of the anchor; its transform will now benefit
+            // from the anchor's tracking.
+            var placedObject = Instantiate(PlaceGameObject, hit.Pose.position, hit.Pose.rotation);
+            //Each new cube will get a new color upon instantiation.
+			placedObject.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+			// Make the newly placed object a child of the parent
+			placedObject.transform.parent = anchor.transform;
+		}
+	}
+}
---
+++ B_HitTestingARCore/ARCore1.2/README.md
@@ -0,0 +1,13 @@
+#Assets for HitTesting tutorial using ARCore 1.2.
+
+ChangeLog - TrackedPlane Prefab has now been renamed to DetectedPlaneVisualizer
+and can be found at:
+GoogleARCore>>Examples>>Common>>Prefabs>>DetectedPlaneVisualizer
+Use the PlaneVisualizationManager.cs from the ARCore1.2 Folder
+InstantiatObjectOnTouch.cs Code Changed => GoogleARCore.HelloAR; to GoogleARCore.Examples.HelloAR; 
+
+If you are using ARCore 1.1 or 1.0, You will find this at:
+GoogleARCore>>Examples>>HelloAR>>Prefabs>>TrackedPlaneVisualizer
+Use the PlaneVisualizationManager.cs from the ARCore1.0_1.1 Folder
+
+
